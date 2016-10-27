0dc7c6ba8dfc7c2e9e6c21be555cb35b4073cecf
sus111 <sgraham535@gmail.com>
2016-10-27 16:15:26 +0100

    delete posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ const API_KEY = '?key=kjkljnkiweofioa321jd'
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -30,6 +31,15 @@ export function fetchPost(id) {
 
   return {
     type: FETCH_POST,
+    payload: request
+  }
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
