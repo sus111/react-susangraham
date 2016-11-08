4aa49ca12993c8221edab216ca16fb285d014ca9
sus111 <sgraham535@gmail.com>
2016-11-08 20:55:30 +0000

    bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path')
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: (__dirname, '/dist'),
+    path: path.join(__dirname, '/dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
